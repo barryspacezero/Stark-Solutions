db445aa55a96b0e7043de833c934c32ebf97694b
barryspacezero <barryspace01@gmail.com>
2024-07-21 00:55:13 +0530

    Added About Section, also made cards.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-////
+gsap.to("nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "50px",
+    scrollTrigger: {
+      trigger: "nav",
+      scroller: "body",
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
